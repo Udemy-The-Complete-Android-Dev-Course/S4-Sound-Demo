fba37c1c00e13ba66d9c1fb138d318466befaad5
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-17 13:15:43 +0100

    Add a progress bar to control the playing location in the audio file.

--- app/src/main/java/ldansorean/s4sounddemo/MainActivity.java
+++ app/src/main/java/ldansorean/s4sounddemo/MainActivity.java
@@ -10,13 +10,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static android.media.AudioManager.STREAM_MUSIC;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private AudioManager audioManager;
-    private SeekBar volumeSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,45 @@ public class MainActivity extends AppCompatActivity {
 
         mediaPlayer = MediaPlayer.create(this, R.raw.rainforest);
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        volumeSeekBar = findViewById(R.id.volume);
 
-        //setup volume controls
+        setupVolumeSeeker();
+        setupProgressSeeker();
+    }
+
+    private void setupProgressSeeker() {
+        final SeekBar progressSeekBar = findViewById(R.id.progress);
+        progressSeekBar.setMax(mediaPlayer.getDuration());
+
+        //seek bar should be updated as song keeps playing
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                progressSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+
+        //when manually updating seekbar should jump to the position in the audio file
+        progressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.start();
+            }
+        });
+    }
+
+    private void setupVolumeSeeker() {
+        SeekBar volumeSeekBar = findViewById(R.id.volume);
+
         int maxVolume = audioManager.getStreamMaxVolume(STREAM_MUSIC);
         int currentVolume = audioManager.getStreamVolume(STREAM_MUSIC);
         volumeSeekBar.setMax(maxVolume);
@@ -36,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("seekbar", Integer.toString(progress));
                 audioManager.setStreamVolume(STREAM_MUSIC, progress, 0);
             }
 
