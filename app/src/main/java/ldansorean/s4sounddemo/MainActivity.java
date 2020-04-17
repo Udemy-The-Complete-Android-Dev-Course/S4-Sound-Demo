dbeab70fb1a7ec3ca63925d2f97c41358aad81bd
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-17 01:30:56 +0100

    Add an audio that can be played and paused.

--- app/src/main/java/ldansorean/s4sounddemo/MainActivity.java
+++ app/src/main/java/ldansorean/s4sounddemo/MainActivity.java
@@ -2,13 +2,27 @@ package ldansorean.s4sounddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.rainforest);
+    }
+
+    public void play(View view) {
+        mediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
     }
 }
